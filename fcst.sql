cf5b05e504ff1d156b13d30f8a471669137b1cb4
mayurstack-alt <thisismayur18@gmail.com>
2025-10-28 12:07:16 +0530

    Replaced old project with new files

--- fcst.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE routes (
-    id SERIAL PRIMARY KEY,
-    source VARCHAR(50),
-    destination VARCHAR(50),
-    cost INT,
-    algorithm_used VARCHAR(30)
-);
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const readline = require('readline');
-
-const INF = 9999;
-const MAX = 10;
